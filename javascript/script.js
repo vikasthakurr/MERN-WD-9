998c025f9156401f5f2b6cfc2e924c2c0cb38691
vikasthakurr <vikaskumar20012001@gmail.com>
2025-07-09 19:19:52 +0530

    variable in js

---
+++ javascript/script.js
@@ -0,0 +1,35 @@
+// var a = 10;
+
+// console.log(a);
+
+// function square(n) {
+//   return n * n;
+// }
+
+// var result1 = square(6);
+// console.log(result1);
+
+// var result2 = square(a);
+// console.log(result2);
+// //
+
+var a = 10;
+
+function a() {
+  var b = 20;
+  console.log(b);
+
+  function b() {
+    var c = 30;
+    console.log(c);
+
+    function c() {
+      var c = 60;
+      console.log(c);
+    }
+    c();
+  }
+  b();
+}
+a();
+console.log(a);
