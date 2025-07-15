a5627218677e1eda525429638893b620189f41e7
vikasthakurr <vikaskumar20012001@gmail.com>
2025-07-15 21:43:10 +0530

    function and its scope

---
+++ javascript/function.js
@@ -0,0 +1,28 @@
+//Function Declaration
+
+function greet(name) {
+  console.log(`Hello ${name}!`);
+}
+
+//Function Expression
+
+const greet = function (name) {
+  console.log(`Hello ${name}!`);
+};
+
+//Arrow Function
+
+const greet = (name) => {
+  console.log(`Hello ${name}!`);
+};
+
+//Immediately Invoked Function Expression (IIFE)
+
+(function () {
+  console.log("Hello World!");
+})();
+
+
+
+
+
--- javascript/scope.js
+++ javascript/scope.js
@@ -1,53 +1,60 @@
-//global scope
-// let a = 10;
+//Global Scope
+// Variables declared outside of any function or block have Global Scope.
+// They can be accessed from anywhere in the code.
 
-// function print() {
-//   console.log(a);
-// }
-// print();
-// console.log(a);
+let globalA = 10; // This is a global variable
 
-//function scope
+function printGlobal() {
+  console.log("Inside function:", globalA); // Accessible inside a function
+}
 
-// function print() {
-//   const a = 10;
-//   console.log(a);
-// }
-// print();
-// console.log(a);
+printGlobal();
+console.log("Outside function:", globalA); // Accessible in the global scope
 
-//block scope
-// {
-//   let a = 10;
-//   const b = 20;
-//   var c = 60;
-// }
-// console.log(a);
-// console.log(b);
+// ---
 
-// console.log(c);
+// 2. Function Scope
+// Variables declared inside a function are only accessible within that function.
 
-// function outer() {
-//   function inner1() {
-//     console.log(a);
-//   }
-//   function inner2() {
-//     let a = 10;
-//     console.log(a);
-//   }
-//   inner1();
-//   inner2();
-// }
-// outer();
+function functionScopeExample() {
+  const functionScopedVar = "I am inside the function";
+  console.log(functionScopedVar); // Works perfectly
+}
 
-//lexical env
-let a = 30;
-function outer() {
-  function inner() {
-    function inner2() {
-      console.log(a);
-    }
-    inner2();
+functionScopeExample();
+
+// This will cause an error because `functionScopedVar` does not exist in the global scope.
+// console.log(functionScopedVar); // ReferenceError: functionScopedVar is not defined
+
+
+//3. Block Scope
+// Variables declared with `let` and `const` inside a block `{}` are only accessible within that block.
+// `var` is NOT block-scoped.
+
+{
+  let blockLet = 10;
+  const blockConst = 20;
+  var blockVar = 60; // 'var' is function-scoped or global-scoped
+  console.log("Inside block (let):", blockLet);
+  console.log("Inside block (const):", blockConst);
+  console.log("Inside block (var):", blockVar);
+}
+
+// console.log("Outside block (let):", blockLet); // ReferenceError: blockLet is not defined
+// console.log("Outside block (const):", blockConst); // ReferenceError: blockConst is not defined
+console.log("Outside block (var):", blockVar); // Prints 60. 'var' "leaks" out of the block.
+
+//4. Lexical Scope (Lexical Environment) 
+// An inner function has access to the variables of its outer functions.
+// This is called lexical scoping. The "scope chain" goes from inner to outer.
+
+let lexicalVar = 30; // Global scope
+
+function outer() { // Outer scope
+  let outerVar = "Outer";
+  function inner() { // Inner scope
+    // `inner` can "see" `outerVar` and `lexicalVar` because of the scope chain.
+    console.log("From inner function:", outerVar, "&", lexicalVar);
   }
   inner();
 }
