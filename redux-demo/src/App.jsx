e54913df204f39f5e3c0e68a8524d7c7138744aa
vikasthakurr <vikaskumar20012001@gmail.com>
2025-08-20 18:35:02 +0530

    redux

---
+++ redux-demo/src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import AddTodo from "./AddTodo";
+import ViewTodo from "./ViewTodo";
+
+const App = () => {
+  return (
+    <>
+      <AddTodo />
+      <br></br>
+      <br></br>
+      <br></br>
+      <ViewTodo />
+    </>
+  );
+};
+
+export default App;
